12c95174eb3542c3dc6c7e14b4181e6a3e464838
Mahmudul107 <mahmudulislam378@gmail.com>
2023-06-09 20:54:49 +0600

    marque added in the homepage as extra section

--- src/Pages/Home/Home/Marque/Marque.jsx
+++ src/Pages/Home/Home/Marque/Marque.jsx
@@ -5,8 +5,17 @@ const Marque = () => {
   return (
     <div className="flex w-4/5 mx-auto">
       <button className="bg-red-400 btn"></button>
-
-      
+      <Marquee
+        className="text-red-500 text-2xl"
+        gradient={true}
+        pauseOnHover={true}
+        autoFill={true}
+        speed={80}
+      >
+        Join our Dance Verse and Embark on a Journey of Passion, Grace, and
+        Artistic Expression. Explore a World of Rhythm, Movement, and Joyful
+        Performances. Unleash Your Potential and Dance to Your Heart's Content!
+      </Marquee>
     </div>
   );
 };
