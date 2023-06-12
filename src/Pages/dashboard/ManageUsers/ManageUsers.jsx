36ad4139d2490efe544fa7b5bc349ea067e8e417
Mahmudul107 <mahmudulislam378@gmail.com>
2023-06-12 21:28:53 +0600

    made an admin successfully

--- src/Pages/dashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/dashboard/ManageUsers/ManageUsers.jsx
@@ -1,28 +1,36 @@
-import { useQuery } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
-  const { data: users = [], refetch } = useQuery(['users'], async () => {
-    const res = await fetch('http://localhost:5000/users');
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
 
   const [selectedUser, setSelectedUser] = useState(null);
 
   const makeInstructor = (user) => {
-    // Implementation to make the user an instructor
-    // ...
-
-    // Example implementation:
-    // ...
+   
   };
 
   const makeAdmin = (user) => {
-    // Implementation to make the user an admin
-    // ...
-
-    // Example implementation:
-    // ...
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+        method: 'PATCH'
+    })
+    .then( res => res.json())
+    .then(data=> {
+        if(data.modifiedCount) {
+            refetch()
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: `${user.name} is an Admin now!`,
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+    })
   };
 
   return (
@@ -30,11 +38,17 @@ const ManageUsers = () => {
       <h3 className="text-2xl font-bold mb-4">Manage Users Page</h3>
       {users.length > 0 ? (
         <table className="w-[900px] border border-gray-300">
-          <thead>
+          <thead className="bg-red-100">
             <tr>
-              <th className="border-b-2 border-gray-300 px-4 py-2 border-l">Image</th>
-              <th className="border-b-2 border-gray-300 px-4 py-2 border-l">Name</th>
-              <th className="border-b-2 border-gray-300 px-4 py-2 border-l border-r">Email</th>
+              <th className="border-b-2 border-gray-300 px-4 py-2 border-l">
+                Image
+              </th>
+              <th className="border-b-2 border-gray-300 px-4 py-2 border-l">
+                Name
+              </th>
+              <th className="border-b-2 border-gray-300 px-4 py-2 border-l border-r">
+                Email
+              </th>
               <th className="border-b-2 border-gray-300 px-4 py-2">Actions</th>
             </tr>
           </thead>
@@ -43,27 +57,43 @@ const ManageUsers = () => {
               <tr
                 key={user._id}
                 className={`${
-                  index % 2 === 0 ? 'bg-gray-100' : 'bg-white'
+                  index % 2 === 0 ? "bg-gray-100" : "bg-white"
                 } hover:bg-slate-100`}
               >
                 <td className="border-b border-gray-300 px-4 py-2 border-l">
-                  <img src={user.photoURL} alt="User" className="w-10 h-10 rounded-full" />
+                  <img
+                    src={user.photoURL}
+                    alt="User"
+                    className="w-10 h-10 rounded-full"
+                  />
                 </td>
-                <td className="border-b border-gray-300 px-4 py-2 border-l">{user.name}</td>
-                <td className="border-b border-gray-300 px-4 py-2 border-l border-r">{user.email}</td>
-                <td className="border-b border-gray-300 px-4 py-2">
-                  <button
-                    onClick={() => makeInstructor(user)}
-                    className="text-sm px-4 py-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-600"
-                  >
-                    Make Instructor
-                  </button>
-                  <button
-                    onClick={() => makeAdmin(user)}
-                    className="text-sm px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600 ml-2"
-                  >
-                    Make Admin
-                  </button>
+                <td className="border-b border-gray-300 px-4 py-2 border-l">
+                  {user.name}
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2 border-l border-r">
+                  {user.email}
+                </td>
+                <td className="border-b border-gray-300 px-4 py-2 text-end flex justify-evenly">
+                  {user.role === "instructor" ? (
+                    "instructor"
+                  ) : (
+                    <button
+                      onClick={() => makeInstructor(user)}
+                      className="text-sm px-4 py-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-600"
+                    >
+                      Make Instructor
+                    </button>
+                  )}
+                  {user.role === "admin" ? (
+                    "admin"
+                  ) : (
+                    <button
+                      onClick={() => makeAdmin(user)}
+                      className="text-sm px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600 ml-2"
+                    >
+                      Make Admin
+                    </button>
+                  )}
                 </td>
               </tr>
             ))}
