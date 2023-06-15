f50cccd0e2b3a9c36462c35718c1f4b7f7e0da3d
Mahmudul107 <mahmudulislam378@gmail.com>
2023-06-15 11:23:32 +0600

    popular instructors page updated

--- src/Pages/Home/Home/PopularInstructors/PopularInstructors.jsx
+++ src/Pages/Home/Home/PopularInstructors/PopularInstructors.jsx
@@ -4,13 +4,10 @@ const PopularInstructors = () => {
   const [instructors, setInstructors] = useState([]);
 
   useEffect(() => {
-    fetch("instructors.json")
+    fetch("http://localhost:5000/users/instructor")
       .then((response) => response.json())
       .then((data) => {
-        const popularInstructors = data.filter(
-          (instructor) => instructor.category === "Popular"
-        );
-        setInstructors(popularInstructors);
+        setInstructors(data.slice(0, 6)); 
       });
   }, []);
 
@@ -24,17 +21,17 @@ const PopularInstructors = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
         {instructors.map((instructor) => (
           <div
-            key={instructor.id}
-            className="bg-white rounded shadow p-4 flex flex-col relative"
+            key={instructor._id}
+            className="bg-white w-96 rounded shadow p-4 flex flex-col relative"
           >
             <img
-              src={instructor.image}
-              alt={instructor.instructorName}
+              src={instructor.photoURL}
+              alt={instructor.name}
               className="w-full object-cover rounded mb-4 flex-grow"
             />
             <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-500 hover:opacity-100">
               <h3 className="text-4xl font-semibold text-cyan-50 bg-red-400 p-4 rounded-md">
-                {instructor.instructorName}
+                {instructor.name}
               </h3>
             </div>
           </div>
--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -7,21 +7,25 @@ const Instructor = () => {
   return (
     <div className='w-[1400px] mx-auto my-20'>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-      {instructors.map((instructor) => (
-        <div
-          key={instructor._id}
-          className="bg-white rounded shadow p-4 flex flex-col items-center"
-        >
-          <img
-            src={instructor.photoURL}
-            alt="Instructor"
-            className="w-72 h-72 rounded-full mb-4"
-          />
-          <h1 className="text-xl font-bold mb-2">{instructor.name}</h1>
-          <p className="text-gray-600">{instructor.email}</p>
-        </div>
-      ))}
-    </div>
+        {instructors.map((instructor) => (
+          <div
+            key={instructor._id}
+            className="bg-gradient-to-r from-red-100 to-slate-200 rounded-xl shadow p-4 flex flex-col items-center"
+          >
+            <img
+              src={instructor.photoURL}
+              alt="Instructor"
+              className="w-72 h-72 rounded-full mb-4"
+            />
+            <h1 className="text-xl font-bold mb-2">
+              <span className="text-purple-600 text-lg">Name: </span> {instructor.name}
+            </h1>
+            <p className="text-gray-600">
+              <span className="text-green-600">Email: </span> {instructor.email}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
